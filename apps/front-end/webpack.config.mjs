d2b724c3fa423883c6883264faefd031cca3142e
XYShaoKang <xyshaokang@gmail.com>
2021-08-27 07:56:26 +0800

    chore(front-end): mock api
    添加开发用的模拟 API

--- apps/front-end/webpack.config.mjs
+++ apps/front-end/webpack.config.mjs
@@ -76,6 +76,48 @@ const developmentConfig = {
     port: 9000,
     open: true,
     hot: true,
+    /**
+     *
+     * @param {import('webpack-dev-server')} devServer
+     */
+    onBeforeSetupMiddleware: function (devServer) {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined')
+      }
+
+      const STATIC_PATH = path.join(__dirname, './static')
+
+      devServer.app.get('/dir', function (req, res) {
+        const { relativePath } = req.query
+        if (relativePath === '/') {
+          res.json({
+            msg: 'ok',
+            children: [
+              {
+                name: 'demo.mp4',
+                mimetype: 'video/mp4,',
+                path: '/demo.mp4',
+              },
+            ],
+          })
+        } else {
+          res.json({
+            error: 'no found directory',
+          })
+        }
+      })
+      devServer.app.get('/file', function (req, res) {
+        const { relativePath } = req.query
+        if (relativePath === '/demo.mp4') {
+          const fileName = path.join(STATIC_PATH, relativePath)
+          res.sendFile(fileName)
+        } else {
+          res.json({
+            error: 'no found file',
+          })
+        }
+      })
+    },
   },
   plugins: [new ReactRefreshWebpackPlugin()],
   devtool: 'eval-source-map',
