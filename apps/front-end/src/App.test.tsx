c24bf764da3646fb21d8bb414806e642043c5fe4
XYShaoKang <xyshaokang@gmail.com>
2021-08-29 04:02:59 +0800

    test(front-end): add test

--- apps/front-end/src/App.test.tsx
+++ apps/front-end/src/App.test.tsx
@@ -1,7 +1,44 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App'
 
-it('App render', () => {
-  render(<App />)
-  expect(screen.getByText(/Hello world!/i)).toBeInTheDocument()
+it('App render', async () => {
+  const spiedFetch = jest.spyOn(window, 'fetch')
+  spiedFetch
+    .mockResolvedValueOnce(
+      Promise.resolve(
+        new Response(
+          `{"msg":"ok","children":[{"name":"demo.mp4","mimetype":"video/mp4","path":"/demo.mp4","modified":"2021-08-26T13:27:41.750Z","size":12228098},{"name":"temp","isDirectory":true,"path":"/temp","modified":"2021-08-26T13:27:41.750Z"}]}`,
+          { status: 200, headers: { 'Content-type': 'application/json' } }
+        )
+      )
+    )
+    .mockResolvedValueOnce(
+      Promise.resolve(
+        new Response(`{"msg":"ok","children":[]}`, {
+          status: 200,
+          headers: { 'Content-type': 'application/json' },
+        })
+      )
+    )
+
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+
+  await waitFor(() => {
+    expect(screen.getByText(/root/i)).toBeInTheDocument()
+    expect(screen.getByText(/temp/i)).toBeInTheDocument()
+  })
+
+  userEvent.click(screen.getByText(/temp/i))
+
+  await waitFor(() => {
+    expect(screen.getByText(/temp/i)).toBeInTheDocument()
+  })
+  expect(screen.queryByText(/demo\.mp4/i)).not.toBeInTheDocument()
 })
