f8b4b908d53dd5eb9738682e5a4038f1c9027807
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-08-29 21:35:42 +0800

    refactor(front-end): extract fetch info logic

--- apps/front-end/src/Main.tsx
+++ apps/front-end/src/Main.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC } from 'react'
 import styled from 'styled-components/macro'
 import { useLocation } from 'react-router-dom'
 
@@ -6,6 +6,7 @@ import Nav from './Nav'
 import Link from './Link'
 import List from './List'
 import Error from './Error'
+import useInfo from './hooks/useInfo'
 
 const Container = styled.div`
   width: 800px;
@@ -22,77 +23,15 @@ const Header = styled.div`
   background: #f2f2f2;
 `
 
-interface FileType {
-  name: string
-  isDirectory: false
-  path: string
-  modified: string
-  mimetype: string
-  size: number
-}
-interface DirectoryType {
-  name: string
-  isDirectory: true
-  path: string
-  modified: string
-}
-
-type ItemInfo = (FileType | DirectoryType) & {
-  children?: Array<FileType | DirectoryType>
-}
-
-type StateType = Array<{
-  name: string
-  isDirectory: boolean
-  path: string
-  modified: string
-  mimetype: string
-  size?: number
-}>
-
 const Directory: FC = () => {
   const { pathname } = useLocation()
 
-  const [children, setChildren] = useState<StateType>([])
-  const [currentInfo, setCurrentInfo] = useState<FileType | DirectoryType>()
-  const [error, setError] = useState<string>()
+  const { children, currentInfo, error } = useInfo(pathname)
 
   const path = error ? pathname : currentInfo?.path
 
   const dirs = path?.slice(1).split('/').filter(Boolean) ?? []
   dirs.unshift('root')
-
-  useEffect(() => {
-    fetch(`/info?relativePath=${pathname}`)
-      .then(res => res.json())
-      .then(
-        (
-          data:
-            | {
-                msg: string
-                info: ItemInfo
-              }
-            | { error: string }
-        ) => {
-          if ('msg' in data && data.msg === 'ok') {
-            const { children, ...tempInfo } = data.info
-            if (children) {
-              setChildren(
-                children
-                  .map(item =>
-                    item.isDirectory ? { ...item, mimetype: 'Folder' } : item
-                  )
-                  .sort(a => (a.isDirectory ? -1 : 1))
-              )
-            }
-            setCurrentInfo(tempInfo)
-            setError(undefined)
-          } else if ('error' in data) {
-            setError(`获取 ${pathname} 目录数据失败,请检查路径`)
-          }
-        }
-      )
-  }, [pathname])
 
   return (
     <Container>
