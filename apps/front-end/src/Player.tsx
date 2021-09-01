a24829e4d57321041286194bc05991c1fe4df6e8
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-09-01 23:29:33 +0800

    fix(front-end): prevent overflow of lists that are too long

--- apps/front-end/src/Player.tsx
+++ apps/front-end/src/Player.tsx
@@ -7,6 +7,9 @@ import { FileType } from './hooks/useInfo'
 const PlayerWrap = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 `
 
 const PlayerRoot = styled.div`
