b43379b7915bde3682e268db2a757a451a7354f0
XYShaoKang <xyshaokang@gmail.com>
2021-09-01 22:16:35 +0800

    feat(video-http-server): add configure root directory
    使用当前命令行所在的工作目录,也就是 pwd 的结果作为基础去计算要使用的根目录
    默认`root`选项为`./`,也就是默认使用命令行执行时的工作目录

--- apps/video-http-server/src/server.ts
+++ apps/video-http-server/src/server.ts
@@ -2,27 +2,38 @@ import path from 'path'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import serve from 'koa-static'
+import http from 'http'
+
 import { createRoute } from './router'
 
 const CLIENT_PATH = path.join(__dirname, '../../front-end/dist/')
 
-const ROOT_PATH = path.join(__dirname, '../videos/')
-const router = createRoute(ROOT_PATH)
+function createApp(rootPath: string): Koa {
+  // const ROOT_PATH = path.join(__dirname, '../videos/')
+  const router = createRoute(rootPath)
 
-const app = new Koa()
+  const app = new Koa()
 
-app.use(bodyParser())
-app.use(serve(CLIENT_PATH))
-app.use(router.routes()).use(router.allowedMethods())
+  app.use(bodyParser())
+  app.use(serve(CLIENT_PATH))
+  app.use(router.routes()).use(router.allowedMethods())
 
-app.on('error', error => {
-  // TODO: 暂时先使用警告来代替错误
-  // https://github.com/koajs/koa/issues/1089
-  if (error.code === 'EPIPE') {
-    console.warn('Koa app-level EPIPE error.', { error })
-  } else {
-    console.error('Koa app-level error', { error })
-  }
-})
+  app.on('error', error => {
+    // TODO: 暂时先使用警告来代替错误
+    // https://github.com/koajs/koa/issues/1089
+    if (error.code === 'EPIPE') {
+      console.warn('Koa app-level EPIPE error.', { error })
+    } else {
+      console.error('Koa app-level error', { error })
+    }
+  })
+  return app
+}
 
-export { app }
+function createServer(rootPath: string): http.Server {
+  const app = createApp(rootPath)
+  const server = http.createServer(app.callback())
+  return server
+}
+
+export { createApp, createServer }
