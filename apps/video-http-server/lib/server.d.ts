da2f96983b9dbab7b1369f659fa9669e2a053003
XYShaoKang <xyshaokang@gmail.com>
2021-09-03 03:09:50 +0800

    build(video-http-server): remove lib from git

--- apps/video-http-server/lib/server.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="koa-bodyparser" />
-/// <reference types="node" />
-import Koa from 'koa';
-import http from 'http';
-declare function createApp(rootPath: string): Koa;
-declare function createServer(rootPath: string): http.Server;
-export { createApp, createServer };
